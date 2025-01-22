f320ad4e03bf23227eefceda04b55891df4a4342
Wouter Zomervrucht <149009213+wzomervrucht@users.noreply.github.com>
2025-01-22 20:11:05 +0100

    puzzle config

--- puzzles/puzzle.ts
+++ puzzles/puzzle.ts
@@ -1,9 +1,9 @@
-export interface Puzzle {
+export interface Puzzle<in TConfig = never> {
   day: number;
   title: string;
   input: string;
-  solve1: (input: string[]) => string | number;
-  solve2: (input: string[]) => string | number;
+  solve1: (input: string[], config?: TConfig) => string | number;
+  solve2: (input: string[], config?: TConfig) => string | number;
   answer1: string | number;
   answer2: string | number;
 }
